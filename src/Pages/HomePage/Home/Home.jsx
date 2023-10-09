ad6c41f065f7710458816f43097d2f3bc3e28219
tahsif <tahsif.cse@gmail.com>
2023-10-10 00:08:57 +0600

    add aos

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,23 +1,31 @@
- 
+// Import necessary libraries and styles
+import  { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the CSS
+// import './Home.css'; // Import your custom CSS file if needed
+
+// Import your custom components
 import Banner from "../Banner/Banner";
 import ChooseEvent from "../ChooseEvent/ChooseEvent";
- 
 import Services from "../Services/Services";
 import Stories from "../Stories/Stories";
 
+function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // Animation duration (in milliseconds)
+    });
+  }, []);
 
-const Home = () => {
-    return (
-        <div>
-         
-             
-            <Banner></Banner>
-            <Services></Services>
-            <ChooseEvent></ChooseEvent>
-            <Stories></Stories>
-          
-        </div>
-    );
-};
+  return (
+    <div>
+      {/* Components with data-aos attributes */}
+      <Banner data-aos="fade"></Banner>
+      <Services data-aos="slide-up"></Services>
+      <ChooseEvent data-aos="fade"></ChooseEvent>
+      <Stories data-aos="slide-up"></Stories>
+    </div>
+  );
+}
 
 export default Home;
